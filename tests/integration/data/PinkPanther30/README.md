6581413b4a54f53782657441f3eb21c90456ad37
moreaupascal56 <moreaupascal56@gmail.com>
2024-08-24 11:59:00 +0200

    add integration test

---
+++ tests/integration/data/PinkPanther30/README.md
@@ -0,0 +1,14 @@
+This data have been created using Audacity markers and export multiple files option. 
+This results in the warning below when executing tests:
+
+```
+
+tests/integration/test_int_wavmerger.py::test_concat_wav_in_dir
+  /home/pascal/Documents/github/wavsurfer/wavmerger/wavmerger.py:21: WavFileWarning: Chunk (non-data) not understood, skipping it.
+    self.rate, self.data = wavfile.read(filepath)
+
+-- Docs: https://docs.pytest.org/en/stable/how-to/capture-warnings.html
+
+```
+
+Please ignore it.
---
+++ tests/integration/test_int_wavmerger.py
@@ -0,0 +1,52 @@
+import pytest
+
+from tests.helpers import WavFileEquality
+from wavmerger import concat_wav_in_dir, WavFile
+from pathlib import Path
+
+
+@pytest.fixture
+def get_wav_test_data() -> tuple[Path,Path]:
+    """Return the test data path. The test data is an original .wav file and the same file
+    splitted in several files in the splitted/ directory.
+
+    Returns:
+        tuple[Path,Path]: (original .wav path, directory containingthe splitted .wav files)
+    """
+    dirpath = Path(__file__).parent.joinpath("data").joinpath("PinkPanther30")
+    return (
+        dirpath.joinpath("PinkPanther30.wav"),
+        dirpath.joinpath("splitted")
+    )
+
+
+
+
+@pytest.mark.filterwarnings("ignore") # A warning is raised because of the way the test data is created, let's ignore it
+def test_concat_wav_in_dir(
+    get_wav_test_data, temp_dir
+):
+    """Tests concat_wav_in_dir function
+
+    Args:
+        get_wav_test_data (_type_): fixture
+        temp_dir (_type_): fixture
+    """
+    
+    write_file_name = "output_test_concat_wav_in_dir.wav"
+    original_data_path,splitted_data_path = get_wav_test_data
+    
+    concat_wav_in_dir(
+        dirpath = splitted_data_path,
+        write_file_name = write_file_name,
+        write_file_dir = temp_dir
+    )
+    
+    created_wav = Path(temp_dir).joinpath(write_file_name)
+    
+    WavFileEquality(
+        WavFile(filepath = created_wav),
+        WavFile(filepath = original_data_path),
+    )
+    assert created_wav.is_file()
+    
